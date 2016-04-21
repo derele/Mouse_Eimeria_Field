fa6d4e014e08733ed95340b2c3a6d047009c15d5
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2016-04-21 12:02:51 +0200

    Messy plotting befor sharing data with Jada et al. and code with Alice

--- R/data_combi_ludo_14_15.R
+++ R/data_combi_ludo_14_15.R
@@ -1,4 +1,4 @@
-raw.loc.2014 <- read.csv("../HZ14_Mice 31-12-14_localities.csv")
+raw.loc.2014 <- read.csv("HZ14_Mice 31-12-14_localities.csv")
 raw.loc.2014 <- raw.loc.2014[, c(1:6, 13:15)]
 
 convert.deg <-function(c){
@@ -16,11 +16,12 @@ convert.deg <-function(c){
 raw.loc.2014$Latitude <- convert.deg(raw.loc.2014$Latitude)
 raw.loc.2014$Longitude <- convert.deg(raw.loc.2014$Longitude)
 
-genotypes.2014 <- read.csv("../HZ14_Mice 31-12-14_genotypes.csv")[-1,]
+genotypes.2014 <- read.csv("HZ14_Mice 31-12-14_genotypes.csv")[-1,]
+X.col <- c("X332", "X347", "X65", "Tsx", "Btk",  "Syap1")
+
 
 genotypes.2014$collapsed.GT <-
-    apply(genotypes.2014[, c("X332", "X347", "X65",
-                             "Tsx", "Btk", "Syap1")],
+    apply(genotypes.2014[, X.col],
                          1, paste, collapse = "/")
 
 get.HIX <- function (x){
@@ -41,7 +42,7 @@ loc.HIX.2014 <- get.HIX(
 loc.2014 <- merge(raw.loc.2014, loc.HIX.2014, by.x="Code", by.y = 0)
 
 
-ludo <- read.csv("../dureje_et_al._supplementary_table_s1.csv",
+ludo <- read.csv("dureje_et_al._supplementary_table_s1.csv",
                  skip=1)
 
 ludo$Latitude <- convert.deg(ludo$Latitude)
@@ -59,7 +60,7 @@ area <- get_map(location =
                       max(ludo$Latitude)),
                 source = "stamen", maptype="toner-lite")
 
-png("../figures/Ludo.png", units = "in", width = 6, height = 7, res=300)
+png("figures/Ludo.png", units = "in", width = 6, height = 7, res=300)
 ggmap(area,  zoom = 15) +
     geom_point(data = subset(ludo, !is.na(ludo$HIX)),
                aes(Longitude, Latitude, color=HIX), size=2, alpha=0.5) +
@@ -67,7 +68,7 @@ ggmap(area,  zoom = 15) +
 dev.off()
 
 
-png("../figures/Ludo_plus_Berlin.png", units = "in", width = 6,
+png("figures/Ludo_plus_Berlin.png", units = "in", width = 6,
     height = 7, res=300)
 ggmap(area,  zoom = 15) +
     geom_point(data = subset(ludo, !is.na(ludo$HIX)),
@@ -85,7 +86,7 @@ area.B <- get_map(location =
                         max(loc.2014$Latitude)),
                 source = "stamen", maptype="toner-lite")
 
-png("../figures/Berlin.png", units="in", width = 6, height = 6,
+png("figures/Berlin.png", units="in", width = 6, height = 6,
     res=300)
 ggmap(area.B,  zoom = 15) +
             geom_point(data = subset(loc.2014, !is.na(loc.2014$y)),
@@ -93,28 +94,83 @@ ggmap(area.B,  zoom = 15) +
                 scale_color_gradient("Hybrid\nindex", high="red",low="blue") 
 dev.off()
 
-raw.loc.2015 <- read.csv("../HZ15_Mice_localities.csv")
+raw.loc.2015 <- read.csv("HZ15_Mice_localities.csv")
 
 raw.loc.2015$X_Map <- as.numeric(as.character(raw.loc.2015$X_Map))
 raw.loc.2015$Y_Map <- as.numeric(as.character(raw.loc.2015$Y_Map))
 
 
-png("../figures/Berlin_2015.png", units="in", width = 6, height = 6,  res=300)
-ggmap(area.B,  zoom = 15) +
-    scale_color_gradient("Hybrid\nindex", high="red",low="blue")+
-        geom_point(data = raw.loc.2015,
-                   aes(X_Map, Y_Map), color="green", size=2)+
-            geom_point(data = subset(loc.2014, !is.na(loc.2014$y)),
-                       aes(Longitude, Latitude, color=y), size=2)
-dev.off()
 
-
-png("../figures/Ludo_plus_Berlin_plus_Bavaria.png", units="in", width = 6, height = 7,  res=300)
+png("figures/Ludo_plus_Berlin_plus_Bavaria.png", units="in", width = 6, height = 7,  res=300)
 ggmap(area,  zoom = 15) +
     geom_point(data = raw.loc.2015,
                aes(X_Map, Y_Map), color="green", size=2)+
         geom_point(data = subset(ludo, !is.na(ludo$HIX)),
                    aes(Longitude, Latitude, color=HIX),size=2, alpha=0.5) +
                 geom_point(data = subset(loc.2014, !is.na(loc.2014$y)),
-                           aes(Longitude, Latitude, color=y), size=2, alpha=0.5)+                    scale_color_gradient("Hybrid\nindex", high="red",low="blue")
+                           aes(Longitude, Latitude, color=y), size=2, alpha=0.5)+
+                    scale_color_gradient("Hybrid\nindex", high="red",low="blue")
 dev.off()
+
+
+HZ15G <- read.csv("HZ15_Mice_genotypes.csv")
+
+
+HZ15G$collapsed.GT <-
+    apply(HZ15G[, X.col],
+          1, paste, collapse = "/")
+
+
+HZ15G$HIX <- sapply(HZ15G$collapsed.GT, get.HIX)
+
+
+raw.loc.2015 <- read.csv("HZ15_Mice_localities.csv")
+
+raw.loc.2015$X_Map <- as.numeric(as.character(raw.loc.2015$X_Map))
+raw.loc.2015$Y_Map <- as.numeric(as.character(raw.loc.2015$Y_Map))
+
+
+loc.HIX.2015 <- get.HIX(
+    tapply(HZ15G$collapsed.GT, HZ15G$Code,
+           function (x){
+               paste(x, collapse="/")}))
+
+loc.2015 <- merge(raw.loc.2015, loc.HIX.2015, by.x="Code", by.y = 0)
+
+names(loc.2015)[names(loc.2015)%in%"y"] <- "HIX"
+
+
+png("figures/Ludo_plus_Berlin15_plus_Bavaria.png", units="in", width = 6, height = 7,  res=300)
+ggmap(area,  zoom = 15) +
+        geom_point(data = subset(ludo, !is.na(ludo$HIX)),
+                   aes(Longitude, Latitude, color=HIX),size=2, alpha=0.5) +
+                geom_point(data = subset(loc.2014, !is.na(loc.2014$y)),
+                           aes(Longitude, Latitude, color=y), size=2, alpha=0.5)+
+                    scale_color_gradient("Hybrid\nindex", high="red",low="blue") +
+                        geom_point(data = loc.2015,
+                                   aes(X_Map, Y_Map, color=HIX), size=2, alpha=0.5) 
+dev.off()
+
+
+png("figures/Berlin_2015.png", units="in", width = 6, height = 6,  res=300)
+ggmap(area.B,  zoom = 15) +
+    scale_color_gradient("Hybrid\nindex", high="red",low="blue")+
+        geom_point(data = loc.2015,
+                   aes(X_Map, Y_Map, color=HIX, alpha=0.5))+
+            geom_point(data = subset(loc.2014, !is.na(loc.2014$y)),
+                       aes(Longitude, Latitude, color=y), alpha=0.5)
+dev.off()
+
+
+png("figures/Berlin_2015_labeled.png", units="in", width = 6, height = 6,  res=300)
+ggmap(area.B,  zoom = 15) +
+    scale_color_gradient("Hybrid\nindex", high="red",low="blue")+
+        geom_point(data = loc.2015,
+                   aes(X_Map, Y_Map, color=HIX, alpha=0.5))+
+            geom_point(data = subset(loc.2014, !is.na(loc.2014$y)),
+                       aes(Longitude, Latitude, color=y), alpha=0.5) +
+                geom_text(data = loc.2015,
+                           aes(X_Map, Y_Map, label=Code), size = 1)
+dev.off()
+
+
