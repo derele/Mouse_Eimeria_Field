dd52843df4fe889a222b883720d0fb566ab3872b
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-01-27 14:23:06 +0100

    localities 2014 and 2015 fixed

--- R/database_import.R
+++ R/database_import.R
@@ -3,11 +3,11 @@ source("R/database_import_functions.R")
 
 
 ######## Locations ###############
-raw.loc.2014 <- read.csv("HZ14_Mice 31-12-14_localities.csv")
+raw.loc.2014 <- read.csv("raw_data/HZ14_Mice 31-12-14_localities.csv")
 raw.loc.2014$Latitude <- convert.deg(raw.loc.2014$Latitude)
 raw.loc.2014$Longitude <- convert.deg(raw.loc.2014$Longitude)
 
-raw.loc.2015 <- read.csv("HZ15_Mice_localities.csv")
+raw.loc.2015 <- read.csv("raw_data/HZ15_Mice_localities.csv")
 raw.loc.2015$Longitude <- as.numeric(as.character(raw.loc.2015$X_Map))
 raw.loc.2015$Latitude <- as.numeric(as.character(raw.loc.2015$Y_Map))
 
@@ -26,25 +26,40 @@ loc.consistent <- rbind(loc.consistent, loc.inconsistent[2,])
 
 loc <- loc.consistent[!duplicated(loc.consistent$Code),]
 
+
+write.csv(loc, "output_data/all_clean_localities.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
 ########## DB setup #######################
 
-library(DBI)
-library(RSQLite)
+## library(DBI)
+## library(RSQLite)
 
-## Push to SQLite
-db <- dbConnect(SQLite(), dbname = "/SAN/db/Test.sqlite")
-dbWriteTable(conn = db, name = "Locations", value = loc, row.names = FALSE)
+## ## Push to SQLite
+## db <- dbConnect(SQLite(), dbname = "/SAN/db/Test.sqlite")
+## dbWriteTable(conn = db, name = "Locations", value = loc, row.names = FALSE)
 
-dbListFields(db, "Locations")
+## dbListFields(db, "Locations")
 
-foo <- dbReadTable(db, "Locations")
+## foo <- dbReadTable(db, "Locations")
 
-########## Mouse #########################
-GT.2014 <- read.csv("HZ14_Mice 31-12-14_genotypes.csv")
-DS.2014 <- read.csv("HZ14_Mice 31-12-14_dissections.csv")
+## ########## Mouse #########################
+## GT.2014 <- read.csv("HZ14_Mice 31-12-14_genotypes.csv")
+## DS.2014 <- read.csv("HZ14_Mice 31-12-14_dissections.csv")
 
-## Needed_not <- read.csv("HZ15_Mice_dissections.csv")
-GT.2015 <- read.csv("Genotypes_Bav2015.csv")
-DS.2015 <- read.csv("HZ15_Mice_Parasite.csv")
+## ## Needed_not <- read.csv("HZ15_Mice_dissections.csv")
+## GT.2015 <- read.csv("Genotypes_Bav2015.csv")
+## DS.2015 <- read.csv("HZ15_Mice_Parasite.csv")
 
 
