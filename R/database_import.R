450eb9c24cd6af79ccee266f9ea86ec87ac424b7
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-01-27 15:20:31 +0100

    including 2016 localities

--- R/database_import.R
+++ R/database_import.R
@@ -11,31 +11,46 @@ raw.loc.2015 <- read.csv("raw_data/HZ15_Mice_localities.csv")
 raw.loc.2015$Longitude <- as.numeric(as.character(raw.loc.2015$X_Map))
 raw.loc.2015$Latitude <- as.numeric(as.character(raw.loc.2015$Y_Map))
 
+raw.loc.2016 <- read.csv("raw_data/Cleaned_HMHZ_2016_MmOnly.csv")
+raw.loc.2016$Latitude <- as.numeric(as.character(raw.loc.2016$GPS.coordinates.long))
+raw.loc.2016$Longitude <- as.numeric(as.character(raw.loc.2016$GPS.coordinates.lat))
 
-store <- c("Code", "Continent", "State", "Country",
-           "Locality", "Latitude", "Longitude")
+names(raw.loc.2016)[names(raw.loc.2016)%in%"location"] <- "Code"
 
-loc.raw <- rbind(raw.loc.2014[, store], raw.loc.2015[, store])
+store <- c("Code", "Latitude", "Longitude")
 
-loc.consistent <- do.call(rbind, by(loc.raw, loc.raw$Code, is.geo.consistent))
+loc.raw <- rbind(raw.loc.2014[, store],
+                 raw.loc.2015[, store],
+                 raw.loc.2016[, store])
+
+### Correct a bavarian locality which was labeled like one in Brandenburg
+loc.raw[loc.raw$Code%in%"STEG1" &
+        loc.raw$Latitude%in%"49.8825" &
+        loc.raw$Longitude%in%"11.72"
+       , "Code"] <- "SEYB"
+
+### relable a minor locality with different buildings of the sampe property:
+loc.raw[loc.raw$Code%in%"SCHEN8h" &
+        loc.raw$Latitude%in%"52.273117" &
+        loc.raw$Longitude%in%"13.599425" 
+      , "Code"] <- "SCHEN8f"
+
+### Localities sampled in 2015 by Ludo and Oli in Poland:
+
+
+levels(loc.raw$Code) <- c(levels(loc.raw$Code), paste0("LudPol", 1:3))
+loc.raw[loc.raw$Code%in%"", "Code"] <- paste0("LudPol", 1:3)
+
+loc.consistent <- do.call(rbind, by(loc.raw, loc.raw$Code, is.geo.consistent,
+                                    granularity=0.001))
 
 loc.inconsistent <- do.call(rbind, by(loc.raw, loc.raw$Code,
-                                      is.geo.consistent, consistent=FALSE))
-
-loc.consistent <- rbind(loc.consistent, loc.inconsistent[2,])
+                                      is.geo.consistent, granularity=0.001,
+                                      consistent=FALSE))
 
 loc <- loc.consistent[!duplicated(loc.consistent$Code),]
 
-
 write.csv(loc, "output_data/all_clean_localities.csv")
-
-
-
-
-
-
-
-
 
 
 
