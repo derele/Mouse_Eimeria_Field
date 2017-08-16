84b562b36242dbc581a47f95193aa4e784640837
alicebalard <alice.balard@fu-berlin.de>
2017-08-16 21:53:59 +0200

    add file containing missing info in /raw

--- R/HMHZ_Functions.R
+++ R/HMHZ_Functions.R
@@ -40,23 +40,15 @@ rbind.match.columns <- function(input1, input2) {
 ## Create a genotype dataframe :
 #make.gen.DF <- function(){
   
-  ## Cleaned locations
-  #cleaned_loc <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/output_data/all_clean_localities.csv")
-  #cleaned_loc <- cleaned_loc[-1]
-  #cleaned_loc$Code <- gsub(pattern = "E_", replacement = "", x = cleaned_loc$Code)
-  #cleaned_loc$WhichLoc <- "cleaned"
-  
   # Add the last data received :
   genotypes.2016.and.some.previous <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/raw_data/HIforEH_May2017.csv") 
-  genotypes.2016.and.some.previous$WhichLoc <- "jaroslavcompile"
 
-# All latitude / longitude unknown should be in the file Gen.almost.tot
-  #All_loc <- unique(rbind(unique(data.frame(Code = genotypes.2016.and.some.previous$Code, 
-   #                                         Longitude = genotypes.2016.and.some.previous$Xmap, 
-    #                                        Latitude = genotypes.2016.and.some.previous$Ymap,
-     #                                       WhichLoc = "jaroslavcompile"))))
+  Gen.almost.tot <- data.frame(Mouse_ID = genotypes.2016.and.some.previous$PIN, Code = genotypes.2016.and.some.previous$Code,
+                               Year = genotypes.2016.and.some.previous$Year, HI = genotypes.2016.and.some.previous$HI,
+                               HI_NLoci = genotypes.2016.and.some.previous$HI_NLoci)
+  Gen.almost.tot$Mouse_ID <- gsub(pattern = "SK", replacement = "SK_",x = Gen.almost.tot$Mouse_ID)
   
-  ### Genotypes
+  # Previous data
   genotypes.2014 <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/raw_data/HZ14_Mice%2031-12-14_genotypes.csv")[-1,]
   X.col <- c("X332", "X347", "X65", "Tsx", "Btk",  "Syap1")
   genotypes.2014$Mouse_ID <- paste(genotypes.2014$ID,genotypes.2014$PIN, sep = "_")
@@ -87,41 +79,69 @@ rbind.match.columns <- function(input1, input2) {
   
   Gen14.15 <- Gen14.15[c(7, 8, 9, 11)] 
   Gen14.15$HI_NLoci <- "HI 6"
-  Gen14.15$WhichLoc <- "original"
-  names(Gen14.15) <- c("Mouse_ID", "Code", "Year", "HI", "HI_NLoci", "WhichLoc")
+  names(Gen14.15) <- c("Mouse_ID", "Code", "Year", "HI", "HI_NLoci")
+  
+  # Manual correction 
+  Gen14.15[grep("Sk", Gen14.15$Mouse_ID), ]$Mouse_ID <- "SK_3173"
  
-  #######
-  Gen.almost.tot <- data.frame(Mouse_ID = genotypes.2016.and.some.previous$PIN, Code = genotypes.2016.and.some.previous$Code,
-                               Year = genotypes.2016.and.some.previous$Year, HI = genotypes.2016.and.some.previous$HI,
-                               HI_NLoci = genotypes.2016.and.some.previous$HI_NLoci, WhichLoc = genotypes.2016.and.some.previous$WhichLoc)
-  Gen.almost.tot$Mouse_ID <- gsub(pattern = "SK", replacement = "SK_",x = Gen.almost.tot$Mouse_ID)
-
-  Gen.tot <- merge(Gen.almost.tot, Gen14.15, by = c("Mouse_ID", "Code", "Year", "WhichLoc"), all = TRUE)
-  
-##  https://stackoverflow.com/questions/25622138/conflict-resolution-when-merging-rows-in-r-data-frame
-  
+  # Merge both data frames
+  Gen.tot <- merge(Gen.almost.tot, Gen14.15, by = c("Mouse_ID", "Code", "Year"), all = TRUE)
   
   # By default :
-  Gen.tot$HI <- Gen.tot$HI.y
-  Gen.tot$HI_NLoci <- "HI 6"
+  Gen.tot$HI <- NA
+  Gen.tot$HI_NLoci <- NA
+  Gen.tot[which(!is.na(Gen.tot$HI.y)),]$HI <- Gen.tot[which(!is.na(Gen.tot$HI.y)),]$HI.y
+  Gen.tot[which(!is.na(Gen.tot$HI_NLoci.y)),]$HI_NLoci <- Gen.tot[which(!is.na(Gen.tot$HI_NLoci.y)),]$HI_NLoci.y
   
   # But if "better" HI calculated :
-  Gen.tot[which(is.na(Gen.tot$HI)),]$HI_NLoci <- as.character(Gen.tot[which(is.na(Gen.tot$HI)),]$HI_NLoci.x)
-  Gen.tot[which(is.na(Gen.tot$HI)),]$HI <- Gen.tot[which(is.na(Gen.tot$HI)),]$HI.x
+  Gen.tot[which(!is.na(Gen.tot$HI.x)),]$HI <- Gen.tot[which(!is.na(Gen.tot$HI.x)),]$HI.x
+  Gen.tot[which(!is.na(Gen.tot$HI_NLoci.x)),]$HI_NLoci <- as.character(Gen.tot[which(!is.na(Gen.tot$HI_NLoci.x)),]$HI_NLoci.x)
   
-  # Final :
-  Gen.tot <- Gen.tot[c(1,2,3,4,8,9)]
+  # Remove useless 
+  Gen.tot <- Gen.tot[-(4:7)]
+  Gen.tot <- na.omit(Gen.tot)
+    
+  # Manual correction
+  Gen.tot[which(Gen.tot$Code == "BEETZD"),]$Code <- "BEETZ"
   
-  ## remove duplicates
+  # Check duplicated 
   Gen.tot <- unique(Gen.tot)
+  
+  # Keep best HI if one calculated with more markers
+  Gen.tot$HI_NLoci <- as.numeric(gsub(pattern = "HI ", replacement = "", x = Gen.tot$HI_NLoci))
 
-  Gen.tot$HI <- round(as.numeric(as.character(Gen.tot$HI)), 2)
- 
+  Gen.tot <- Gen.tot[order(Gen.tot$Mouse_ID, -Gen.tot$HI), ] #sort by id and reverse of HI
+  Gen.tot <- Gen.tot[!duplicated(Gen.tot$Mouse_ID), ]              # take the first row within each id
+  
+  # Last check duplicates
+  Gen.tot[which(duplicated(Gen.tot$Mouse_ID)),]
+
   # Add latitude / longitude
-  Gen.fin <- merge(Gen.tot, All_loc, by = c("Code"), all = TRUE)
+  
+  # Cleaned locations
+  #cleaned_loc <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/output_data/all_clean_localities.csv")
+  #cleaned_loc <- cleaned_loc[-1]
+  #cleaned_loc$Code <- gsub(pattern = "E_", replacement = "", x = cleaned_loc$Code)
   
   
-  dup <- Gen.fin[which(duplicated(Gen.fin[c(1,2,3,5,6,7,8)])),]
+  # All latitude / longitude unknown should be in the file Gen.almost.tot
+  All_loc <- unique(rbind(unique(data.frame(Code = genotypes.2016.and.some.previous$Code, 
+                                           Longitude = genotypes.2016.and.some.previous$Xmap, 
+                                          Latitude = genotypes.2016.and.some.previous$Ymap))))
+  
+  # Merge
+  Gen.and.loc <- merge(Gen.tot, All_loc, by = c("Code"), all = TRUE)
+  
+  # For which code and year do we not have the coordinates 
+  Wemissloc <- unique(Gen.and.loc[which(is.na(Gen.and.loc$Longitude)),][c("Code", "Year")])
+  Wemissloc$We_miss <- "Coordinates missing"
+  
+  # For which code and year do we not have the HI 
+  WemissHI <- unique(Gen.and.loc[which(is.na(Gen.and.loc$HI)),][c("Code", "Year")])
+  WemissHI$We_miss <- "HI missing"
+  
+  # To write out :
+  # write.csv(x = rbind(Wemissloc, WemissHI), file = "../output_data/information_missing.csv")
   
   
   df <- Gen.fin[order(Gen.fin[,c("Mouse_ID", "WhichLoc.y")]),]
