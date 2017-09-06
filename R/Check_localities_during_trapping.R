38cd388380775a98506acc0e63678209b9af75c0
alicebalard <alice.balard@fu-berlin.de>
2017-09-06 21:23:35 +0200

    add first draft R script cross checking tables

---
+++ R/Check_localities_during_trapping.R
@@ -0,0 +1,40 @@
+## Alice September 2017
+
+## Import files to cross check
+dissection2017 <- read.csv("../raw_data/HZ17_September_Mice_Dissection")
+trapping2017 <- read.csv("../raw_data/HZ17_Mice_Trap.csv")
+
+## Extract individual coordinates from trapping
+individual_mice_trapped <- unlist(strsplit(as.character(trapping2017$Mice_IDs), " "))
+individual_mice_trapped <- gsub(pattern = " ", replacement = "", x = individual_mice_trapped)
+
+individual_other_trapped <- unlist(strsplit(as.character(trapping2017$Other_samples), " "))
+individual_other_trapped <- gsub(pattern = " ", replacement = "", x = individual_other_trapped)
+
+link_table <- data.frame(Rodent_ID = c(individual_mice_trapped, individual_other_trapped),
+                                        Longitude = NA, Latitude = NA)
+for(i in 1:nrow(link_table)){
+  if(trapping2017[grep(link_table$Rodent_ID[i], trapping2017$Mice_IDs),]$Longitude == TRUE){
+    link_table$Longitude[i] <- trapping2017[grep(link_table$Rodent_ID[i], trapping2017$Mice_IDs),]$Longitude
+  }
+}
+
+
+link_table$Longitude[i] 
+link_table$Rodent_ID %in% trapping2017$Mice_IDs
+  
+  print(c(
+  link_table$Latitude[i] ,
+  trapping2017[grep(link_table$Rodent_ID[i], trapping2017$Mice_IDs),]$Latitude)
+}
+
+
+## Same for fishing in Ratfeces table...
+
+trapping2017$Other_samples
+
+## Cross check matching coordinates and Mouse_ID
+dissection2017$Latitude %in% trapping2017$Latitude
+
+dissection2017$Longitude %in% trapping2017$Longitude
+
