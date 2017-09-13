082ccdfa0b291a56ef46537c5d66971b52128a92
alicebalard <alice.balard@fu-berlin.de>
2017-09-13 21:49:43 +0200

    Add R for mapping mice sampled 13 september

--- R/Check_localities_during_trapping.R
+++ R/Check_localities_during_trapping.R
@@ -28,8 +28,36 @@ correctloc <- function(addresstochange, withnewaddress, codetochange, withnewcod
   dissection2017$Address[which(dissection2017$Code == codetochange)] <- withnewcode
 }
 
+as.character(dissection2017$Code)
+
 correctloc(addresstochange, withnewaddress, codetochange, withnewcode)
 
 ## Write out new dissection table (beware here if done late :P GIT AT ALL STEPS)
 write.csv(x = dissection2017, file = "../raw_data/HZ17_September_Mice_Dissection.csv", row.names = FALSE)
 
+
+###############################
+## Part 2: draw a map
+dissection2017$OldOrNew <- "OldLoc"
+dissection2017[which(dissection2017$Code == ""), ]$OldOrNew <- "NewLoc"
+
+library(ggmap)
+
+dissection2017$Latitude <- as.numeric(dissection2017$Latitude)
+dissection2017$Longitude <- as.numeric(dissection2017$Longitude)
+
+# get a map
+margin <- 0.2
+
+area <- get_map(location =
+                  c(min(na.omit(dissection2017$Longitude) - margin),
+                    min(na.omit(dissection2017$Latitude) - margin),
+                    max(na.omit(dissection2017$Longitude) + margin),
+                    max(na.omit(dissection2017$Latitude) + margin)),
+                source = "stamen", maptype="toner-lite",
+                zoom = 9)
+
+#plot the map :
+ggmap(area) +
+  geom_jitter(data = dissection2017, shape = 21, size = 3,
+             aes(Longitude, Latitude, color = OldOrNew), width =  0.05, height = 0.05)
