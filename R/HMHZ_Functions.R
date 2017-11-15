ca548a97d824b21c80cb4699636917404aaf96ff
Alice Balard <alice.balard@fu-berlin.de>
2017-11-15 15:39:27 +0100

    Add function "measure" in HMHZFunctions.R to convert distance in latitude/longitude to meters

--- R/HMHZ_Functions.R
+++ R/HMHZ_Functions.R
@@ -59,6 +59,18 @@ pairwise.cluster.loc <- function (d){
 ## how many locs are sampled multiple times
 # table(apply(foo, 2, sum))
 
+## Convert distance in latitude/longitude to meters
+measure <- function(lon1,lat1,lon2,lat2) {
+  R <- 6378.137                                # radius of earth in Km
+  dLat <- (lat2-lat1)*pi/180
+  dLon <- (lon2-lon1)*pi/180
+  a <- sin((dLat/2))^2 + cos(lat1*pi/180)*cos(lat2*pi/180)*(sin(dLon/2))^2
+  c <- 2 * atan2(sqrt(a), sqrt(1-a))
+  d <- R * c
+  return (d * 1000)                            # distance in meters
+}
+
+## draw a map with colors depending on HI
 HI.map <- function(df, size = 2, margin = 2, zoom = 6){
   # get a map
   area <- get_map(location =
