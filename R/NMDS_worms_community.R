1c64947a8312894ed34f4c4b0f1911d225da9713
alicebalard <alice.balard@fu-berlin.de>
2017-11-23 15:32:58 +0100

    add NMDS with year

--- R/NMDS_worms_community.R
+++ R/NMDS_worms_community.R
@@ -133,3 +133,63 @@ col[r=="high"] = "darkred"
 ordiplot(my_NMDS,type="n")
 orditorp(my_NMDS,display="species",col="black",air=0.01)
 orditorp(my_NMDS,display="sites",col=col,air=0.01,cex=1.25)
+
+#######################################
+## 3. does it cluster by year?
+# `metaMDS` requires a community-by-species matrix
+worms <- TotalTable[c("Cysticercus", "Trichuris_muris", "Aspiculuris_tetraptera", "Syphacia_obvelata",
+                      "Mastophorus_muris", "Heterakis_spumosa", "Mesocestoides", 
+                      "Catenotaenia_pusilla", "Hymenolepis", "Oxyurids", "Mix_Syphacia_Aspiculuris",
+                      "Heligmosomoides_polygurus", "HI", "Mouse_ID", "Year")]
+# ("Hymenolepis_microstoma", "Hymenolepis_diminiuta") to check, contains "TRUE" and "FALSE"
+
+# remove non mus musculus
+worms <- worms[-grep("ZZ_", worms$Mouse_ID),]
+
+my_community_matrix <- data.matrix(worms)
+dimnames(my_community_matrix)[1] <- list(worms[,"Year"])
+
+# remove rows with only NA for worms (not checked for worms)
+my_community_matrix <-
+  my_community_matrix[rowSums(is.na(my_community_matrix[,c(1:12)]))!= ncol(my_community_matrix[,c(1:12)]), ]
+
+# keep worms only
+my_community_matrix <- my_community_matrix[,c(1:12)]
+
+# then add zeros when NA for worms (absence of worm species checked)
+my_community_matrix[is.na(my_community_matrix)] <- 0
+
+# data normalization : (x - mean)/sd
+my_community_matrix <- apply(my_community_matrix, 2, function(x) {(x - mean(x))/ sd(x)})
+
+# avoid the negative data problem
+my_community_matrix <- my_community_matrix + 1
+
+# The function `metaMDS` will take care of most of the distance 
+# calculations, iterative fitting, etc. We need simply to supply:
+my_NMDS = metaMDS(comm = my_community_matrix, k = 2, maxit = 1000, sratmax = 100)
+##**A good rule of thumb: stress < 0.05 provides an excellent representation in reduced dimensions,
+## < 0.1 is great, < 0.2 is good/ok, and stress < 0.3 provides a poor representation.** 
+
+# Shepard plot, which shows scatter around the regression
+# between the interpoint distances in the final configuration (distances 
+# between each pair of communities) against their original dissimilarities
+stressplot(my_NMDS)
+# Large scatter around the line suggests that original dissimilarities are
+# not well preserved in the reduced number of dimensions
+
+#Now we can plot the NMDS
+plot(my_NMDS)
+
+col = rep("black",dim(my_community_matrix)[1])
+r = rownames(my_community_matrix)
+col[r=="2014"] = "lightgrey"
+col[r=="2015"] = "skyblue"
+col[r=="2016"] = "darkred"
+col[r=="2017"] = "green"
+
+ordiplot(my_NMDS,type="n")
+ordihull(my_NMDS,groups=col,draw="polygon",col=unique(col),label=F)
+orditorp(my_NMDS,display="species",col="grey",air=0.01)
+orditorp(my_NMDS,display="sites",col="black",air=0.01)
+
