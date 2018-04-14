b59e120dc5e8656d6950aa6427f08986a7529eaf
Alice Balard <alice.balard@fu-berlin.de>
2018-04-14 13:57:48 +0200

    Check density along HI & age

--- R/Data_Cleaning_Eimeria_detection.R
+++ R/Data_Cleaning_Eimeria_detection.R
@@ -10,16 +10,23 @@ data2017 <- read.csv("../raw_data/Eimeria_detection/FINAL2017Oocysts.csv")
 
 myData <- rbind(data2015, data2016, data2017)
 
+# Remove mice without OPG
+myData$OPG <- as.numeric(as.character(myData$OPG))
+myData <- myData[!is.na(myData$OPG),]
+
 # add HI, GPS coordinates, BCI (and all)
 myData <- merge(miceTable, myData, by = "Mouse_ID")
 
 # plot
 ggplot(myData) +
-  geom_point(aes(x = HI, y = log10(as.numeric(as.character(OPG)) +1), fill = as.factor(year)),
+  geom_point(aes(x = HI, y = OPG, fill = as.factor(year)),
              pch = 21, alpha = .5, size = 3) +
-  theme_bw() +
-  geom_point(data = myData[is.na(as.numeric(as.character(myData$OPG))),],
-             aes(x = HI, y = 2), pch = "NA") 
+  theme_bw()
+
+ggplot(myData) +
+  geom_point(aes(x = HI, y = log10(OPG)+1, fill = as.factor(year)),
+             pch = 21, alpha = .5, size = 3) +
+  theme_bw()
 
 # Prevalence / year
 prevTable <- as.data.frame.matrix(table(myData$OPG > 0, myData$year))
@@ -27,3 +34,22 @@ prevTable[3,] <- round(prevTable[2,] / colSums(prevTable) *100,2)
 rownames(prevTable)[3] <- "prevalence(%)"
 
 prevTable
+
+# which ones are juveniles? Can we kick them out for the analysis on Eimeria?
+ggplot(myData, aes(x = HI, y = Body_length, col = OPG)) +
+  geom_point() +
+  scale_color_continuous(low = "grey", high = "red") +
+  theme_bw()
+
+myData[myData$Body_length < 60,]
+
+# Density curve : do we catch mice all along the HMHZ?
+ggplot(myData, aes(x = HI))+
+  geom_density(color = "darkblue", fill = "lightblue") +
+  theme_bw()
+# Annoying. The model should get this issue
+# And what about just the infected ones?
+ggplot(myData[myData$OPG>0,], aes(x = HI))+
+  geom_density(color = "darkblue", fill = "lightblue") +
+  theme_bw()
+#same type
