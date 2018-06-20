26458c97d3e21d8b39ad17a3b70254604f180ddd
Alice Balard <alice.balard@fu-berlin.de>
2018-06-20 18:02:02 +0200

    Update after crosschecking Victor + beautiful VennDiagram

--- R/Data_Preparation_Alice.R
+++ R/Data_Preparation_Alice.R
@@ -14,6 +14,10 @@ source("HMHZ_Functions.R")
 # General data
 miceTable <- read.csv("../raw_data/MiceTable_2014to2017.csv")
 
+# Correct HI error
+miceTable$HI[miceTable$HI > 1 & !is.na(miceTable$HI)] <- 
+  miceTable$HI[miceTable$HI > 1 & !is.na(miceTable$HI)]/1000
+
 # Correct Lat/Lon errors
 miceTable$Longitude[miceTable$Longitude > 100 & !is.na(miceTable$Longitude)] <- 
   miceTable$Longitude[miceTable$Longitude > 100 & !is.na(miceTable$Longitude)] / 1000
@@ -49,21 +53,9 @@ myData$year[is.na(myData$year)] <- myData$Year[is.na(myData$year)]
 myData <- subset(myData, select = -c(Year))
 myData$Mouse_ID[is.na(myData$year)] # check, must be null
 
-# MICE NOT FOUND IN miceTable: "SK_3174"
-missingHIMice <- c(as.character(myData$Mouse_ID[
-  !myData$Mouse_ID %in% intersect(miceTable$Mouse_ID, myData$Mouse_ID)]),
-  as.character(myData$Mouse_ID)[is.na(myData$HI)])
-
-prevalenceFlotation <- getPrevalenceTable(myTable = table(myData$OPG > 0,
-                                                          myData$year))
-prevalenceFlotation
-
-# # KEEP ONLY ONES WE FLOTATED!!
-datatomap <- myData[!is.na(myData$OPG) & !is.na(myData$Longitude) & !is.na(myData$Latitude),]
-
-# Create map of samples
-mapHMHZ <- HI.map(df = datatomap, size = 2, alpha = .3, margin = 0.2, zoom = 8) 
-mapHMHZ
+# prevalenceFlotation <- getPrevalenceTable(myTable = table(myData$OPG > 0,
+#                                                           myData$year))
+# prevalenceFlotation
 
 # How many new samples were found given the new dilution (0.1mL)
 N1 <- comparisonFlot(myData)$N1
@@ -108,66 +100,145 @@ myData$EimeriaStatus[myData$PCRstatus == "negative" | myData$OPG <= 0] <- "negat
 # But overwrite positive if one of the test is :)
 myData$EimeriaStatus[myData$PCRstatus == "positive" | myData$OPG > 0] <- "positive"
 
-tabTot <- getPrevalenceTable(table(myData$EimeriaStatus, myData$year))
-tabTot
+# tabTot <- getPrevalenceTable(table(myData$EimeriaStatus, myData$year))
+# tabTot
 
 #################### Eimeria detection qPCR ####################
 source("../R/functions/addqPCRresults.R")
 myData <- addqPCRresults(myData)
 
-# the full values are in myData$delta_ct_MminusE
-tabqpcr <- getPrevalenceTable(table(myData$qPCRstatus, myData$year))
+# # the full values are in myData$delta_ct_MminusE
+# tabqpcr <- getPrevalenceTable(table(myData$qPCRstatus, myData$year))
+# tabqpcr
+
+########### TO CORRECT ########### 
+## Which mice are not found?
+
+# 1. no HI given for these mice
+missingHIMice <- myData$Mouse_ID[is.na(myData$HI)]
+
+# wildpark Schorfheide (not needed, test)
+wsh <- c(paste0("AA_000", 1:9), paste0("AA_00", 10:46))
+# apodemus caught in 2016
+apd <- c("A_0001", "A_0002", "A_0003")
+# useless info
+useless <- c(wsh, apd)
+
+# 2. these mice were not in the initial data from jarda
+notfound <- myData$Mouse_ID[!myData$Mouse_ID %in% miceTable$Mouse_ID]
+
+# 3. total mice missing
+miceInfoNeeded <- unique(as.character(notfound), as.character(missingHIMice))[
+  !unique(as.character(notfound), as.character(missingHIMice)) %in% useless]
 
 #################### General stats on sampling ####################
 
+# Keep mice with OPG, PCR or qPCR status, from the Brandenburg-MVP transect
+myDataStudyAlice <- myData[!myData$Mouse_ID %in% useless & # no wilpark schofheide
+                             (!is.na(myData$OPG) | 
+                                !is.na(myData$PCRstatus) |
+                                !is.na(myData$qPCRstatus)),] # have one of these status
+
+# latitude or longitude missing for mice:
+latLongMissing <- myDataStudyAlice$Mouse_ID[
+  is.na(myDataStudyAlice$Latitude) |
+    is.na(myDataStudyAlice$Longitude)]
+
+# keep only North Germany
+myDataStudyAlice <- myDataStudyAlice[!is.na(myDataStudyAlice$Latitude) &
+                                       myDataStudyAlice$Latitude > 51, ]
+
+# Total
+Nmice <- nrow(myDataStudyAlice)
+Nfarm <- length(unique(myDataStudyAlice$farm))
+
+# Create map of samples
+mapHMHZ <- HI.map(df = myDataStudyAlice, size = 2, alpha = .3, margin = 0.2, zoom = 8) 
+mapHMHZ
+
 # mean and 95% ci of N of mice caught / farm (assuming normal distribution)
-mean(by(myData, myData["farm"], nrow))
-qnorm(0.975)*sd(by(myData, myData["farm"], nrow))/sqrt(nrow(myData))
+MEAN <- mean(by(myDataStudyAlice, myDataStudyAlice["farm"], nrow))
+CI <- qnorm(0.975)*sd(by(myDataStudyAlice, myDataStudyAlice["farm"], nrow))/
+  sqrt(nrow(myDataStudyAlice))
 
 # density of hybrids
-plotDensHI <- ggplot(myData, aes(x = HI)) +
+plotDensHI <- ggplot(myDataStudyAlice, aes(x = HI)) +
   geom_histogram(binwidth = 0.05, col = "black", fill = "lightblue") +
   theme_bw()
 plotDensHI
 
-# Evolution of prevalence / year / farm
-prevFarmYear <- ddply(myData,  .(farm, year), function(x){
-  return(c(prev = sum(x$OPG > 0) / length(x$OPG) * 100,
-           N = length(x$OPG)))})
+# Hybrid index calculation:
+minHINloci = min(as.numeric(substr(myDataStudyAlice$HI_NLoci, 4,6)), na.rm = T)
+maxHINloci = max(as.numeric(substr(myDataStudyAlice$HI_NLoci, 4,6)), na.rm = T)
+meanHINloci = round(mean(as.numeric(substr(myDataStudyAlice$HI_NLoci, 4,6)), na.rm = T))
 
-## Hybrid index calculation:
-minHINloci = min(as.numeric(substr(myData$HI_NLoci, 4,6)), na.rm = T)
-maxHINloci = max(as.numeric(substr(myData$HI_NLoci, 4,6)), na.rm = T)
-meanHINloci = round(mean(as.numeric(substr(myData$HI_NLoci, 4,6)), na.rm = T))
+#Prevalence compared
+prevalenceFlotation <- getPrevalenceTable(table(myDataStudyAlice$OPG > 0, myDataStudyAlice$year))
+prevalencePCR <- getPrevalenceTable(table(myDataStudyAlice$PCRstatus, myDataStudyAlice$year))
+prevalenceqPCR <- getPrevalenceTable(table(myDataStudyAlice$qPCRstatus, myDataStudyAlice$year))
 
 ######### Compare our methods of detection
+noo <- table(!is.na(myDataStudyAlice$OPG))[2]
+npcr <- table(!is.na(myDataStudyAlice$PCRstatus))[2]
+nqPCR <- table(!is.na(myDataStudyAlice$qPCRstatus))[2]
+
 ##Venn diagram 
 
 # first, compare PCR and oocysts
-completeData1 <- myData[!is.na(myData$OPG) &
-                         !is.na(myData$PCRstatus),]
-        
-myVennDiagram <- function(data){      
-  area1 = nrow(subset(completeData1, PCRstatus == "positive"))
-  area2 = nrow(subset(completeData1, OPG > 0))
+completeData1 <- myDataStudyAlice[!is.na(myDataStudyAlice$OPG) &
+                                    !is.na(myDataStudyAlice$PCRstatus),]
+
+myVennDiagram2 <- function(data){      
+  area1 = nrow(subset(data, PCRstatus == "positive"))
+  area2 = nrow(subset(data, OPG > 0))
   ## areas of 2-group overlap
-  cross.area = nrow(subset(completeData1, PCRstatus == "positive" & OPG > 0))
-  
+  cross.area = nrow(subset(data, PCRstatus == "positive" & OPG > 0))
   grid.newpage()
   draw.pairwise.venn(area1 = area1, 
                      area2 = area2, 
                      cross.area = cross.area, 
                      category = c("PCR", "OPG"),
-                     fill = c("blue", "red"), 
-                     alpha = .5, lwd = 0)
+                     col = "transparent", 
+                     fill = c("grey","green"),
+                     alpha = 0.50,                  
+                     cex = 1.5, cat.cex = 1.5, fontfamily = "serif", fontface = "bold",
+                     cat.col = c("grey","green"),
+                     cat.fontfamily = "serif")
 }
 
-myVennDiagram(completeData1)
+myVennDiagram2(completeData1)
 
 # to compare, keep only samples tested for the 3 methods
-completeData <- myData[!is.na(myData$OPG) &
-                         !is.na(myData$PCRstatus) &
-                         !is.na(myData$delta_ct_MminusE),]
+completeData <- myDataStudyAlice[!is.na(myDataStudyAlice$OPG) &
+                                   !is.na(myDataStudyAlice$PCRstatus) &
+                                   !is.na(myDataStudyAlice$qPCRstatus),]
+
+myVennDiagram3 <- function(data){      
+  area1 = nrow(subset(data, PCRstatus == "positive"))
+  area2 = nrow(subset(data, OPG > 0))
+  area3 = nrow(subset(data, qPCRstatus == "positive"))
+  ## areas of 2-group overlap
+  n12 = nrow(subset(data, PCRstatus == "positive" & OPG > 0)) 
+  n23 = nrow(subset(data, qPCRstatus == "positive" & OPG > 0))
+  n13 = nrow(subset(data, PCRstatus == "positive" & qPCRstatus == "positive"))
+  ## areas of 3-group overlap
+  n123 = nrow(subset(data, PCRstatus == "positive" & 
+                       OPG > 0 &
+                       qPCRstatus == "positive"))
+  grid.newpage()
+  draw.triple.venn(area1 = area1, area2 = area2, area3 = area3,
+                   n12 = n12, n23 = n23, n13 = n13,
+                   n123 = n123,
+                   category = c("PCR", "OPG", "qPCR"),
+                   col = "transparent", 
+                   fill = c("grey","green","orange"),
+                   alpha = 0.50,                  
+                   cex = 1.5, cat.cex = 1.5, fontfamily = "serif", fontface = "bold",
+                   cat.col = c("grey","green","orange"),
+                   cat.fontfamily = "serif")
+}
+
+myVennDiagram3(completeData)
 
 #################### BCI by HI and OPG #################### 
 plotBCI <- ggplot(myData, aes(x = HI, y = BCI, fill = myData$OPG + 1)) +
@@ -178,4 +249,3 @@ plotBCI <- ggplot(myData, aes(x = HI, y = BCI, fill = myData$OPG + 1)) +
   theme_bw()
 
 summary(lm(myData$BCI ~ myData$OPG + myData$HI))
-
