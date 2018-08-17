63d500be234024a3164ea4b4b632d8d54c01c8f2
Alice Balard <alice.balard@fu-berlin.de>
2018-08-17 19:00:42 +0200

    Auto stash before merge of "master" and "origin/master"

--- raw_data/Eimeria_detection/raw_qPCR/DataPreparation2017.R
+++ raw_data/Eimeria_detection/raw_qPCR/DataPreparation2017.R
@@ -2,7 +2,7 @@
 rawData <- read.csv("./LorenzoRAW/CSVFiles/TotalLorenzo.csv", stringsAsFactors = F,
                  na.strings = c("NA", "", " ", "-"))
 
-##### Clean data #####
+##### Prepare data #####
 # Column file name
 names(rawData)[names(rawData) %in% "QPCR01.06.2018.XLS.csv"] <- "fileName"
 
@@ -60,65 +60,17 @@ rawData <- rawData[!is.na(rawData$fileName),]
 # manual corrections on sample names
 rawData$Name[rawData$Name %in% "359"] <- "ILWE_AA_0359"
 rawData$Name[rawData$Name %in% "ILWE_AA_242"] <- "ILWE_AA_0242"
+rawData$Name[rawData$Name %in% "CEWE_AA_0330#"] <- "CEWE_AA_0330"
 rawData$Name[rawData$Name %in% "ILWE_AA_0,48"] <- "ILWE_AA_0348"
 rawData$Name[rawData$Name %in% "ILWE_AA_0134"] <- "ILWE_AA_0379"
 rawData[rawData$fileName %in% "QPCR07.06.2018part2.XLS.csv" &
           rawData$Pos %in% paste0("C", 4:12),"Name"] <- gsub("0410", "0409",
                                                              rawData[rawData$fileName %in% "QPCR07.06.2018part2.XLS.csv" &
                                                                        rawData$Pos %in% paste0("C", 4:12),"Name"])
-# likely manual mistake
+rawData[rawData$fileName %in% "QPCR07.06.2018part2.XLS.csv" &
+          rawData$Pos %in% paste0("D", 7:12),"Name"] <- "ILWE_AA_0410" 
 rawData[rawData$Pos %in% c("B4", "B5", "B6") &
           rawData$fileName %in% "QPCR14.06.2018.XLS.csv", "Name"] <- "CEWE_AA_0424"
-
-# # Remove samples with no Ct value
-# rawData <- rawData[!is.na(rawData$Ct.SYBR),]
-# 
-# # Remove samples with no mean Ct (means that only one sample worked)
-# rawData <- rawData[!is.na(rawData$Ct.Mean.SYBR),]
-# 
-
-
-
-
-
-
-
-
-rawData[rawData$fileName %in% "QPCR13.06.2018correct.XLS.csv" &
-          rawData$Pos %in% paste0("F", 7:12), "Name"] 
-
-rawData[rawData$Name %in% "ILWE_AA_0242",]
-
-
-
-nrow(rawData[rawData$fileName %in% "QPCR09.04.2018.XLS.csv",]) # Should be 60 per plate after merging if correct!
-
-myDFcheck <- data.frame(fileName = NA,
-                        rawDataPresent = NA,
-                        rawMeltPresent = NA)
-
-
-
-for (i in rawData$fileName){
-  myDFcheck <- rbind(myDFcheck, data.frame(fileName = i,
-                                           rawDataPresent = is.na(rawData$Target.SYBR[rawData$fileName %in% i]),
-                                           rawMeltPresent = is.na(rawData$No..Tm.SYBR[rawData$fileName %in% i])))
-}
-
-myDFcheck <- unique(myDFcheck)
-
-# rawDataBEFORECHANGES[rawDataBEFORECHANGES$fileName %in% "QPCR09.04.2018.XLS.csv",]
-
-
-
-
-
-
-# Which MeltData not attributed
-rm(rawMeltData)
-
-
-
 
 # Add full name of sample (tissue + mouseID + eimeriaOrmouse primers + plate)
 rawData$fullName <- paste0(rawData$Name, "_", rawData$Target.SYBR, "_",  rawData$fileName)
@@ -128,9 +80,7 @@ x <- strsplit(as.character(rawData$Name), "_", 1)
 rawData$tissue <- sapply( x, "[", 1)
 rawData$Mouse_ID <- paste0("AA_", sapply( x, "[", 3))
 rm(x)
-
-
-##### End cleaning ##### 
+##### END Prepare data #####
 
 ## heatmap to follow
 library(ggplot2)
@@ -169,42 +119,36 @@ myTiles(rawData)
 #                           rawData$No..Tm.SYBR != 0,]
 # myTiles(OKData)
 
-# First,let's check OKData
-## Which samples are complete (mouse+eimeria triplicate, low sd)
-
-
-
-### 1. which ones are at least duplicates? remove the others
+##### Clean data #####
+## 1. Which samples are complete (mouse+eimeria triplicate, low sd)
 # triplicate = same file, same name, same target, same mean
 
 library(dplyr)
 
-sumOKData <- OKData %>% 
+sumOKData <- rawData %>% 
   group_by(fullName) %>% 
   summarise(count = n()) %>% 
   data.frame()
 
-OKData <- OKData[!OKData$fullName %in% 
-                   sumOKData[sumOKData$count < 3, ]$fullName, ]
+OKData <- rawData[!rawData$fullName %in% 
+                   sumOKData[sumOKData$count < 3 , ]$fullName, ]
 myTiles(OKData)
 
 # 2. how are the sd? keep < 3
+badSd <- OKData[!is.na(OKData$Ct.Dev..SYBR) & OKData$Ct.Dev..SYBR > 3 ,]
 OKData <- OKData[OKData$Ct.Dev..SYBR <= 3 ,]
 myTiles(OKData)
 
-# 3. If mice Tm = 0, remove sample
-
-# 3. Mouse AND eimeria  !!!!!!OR just Mouse !!!!!! on the same plate otherwise remove
-OKData$fileNameName <- paste(OKData$fileName, OKData$Name)
-
-sumOKData <- OKData %>% 
-  group_by(fileNameName) %>% 
-  summarise(isBoth = length(unique(Target.SYBR))) %>% 
-  data.frame()
-
-OKData <- OKData[OKData$fileNameName %in% sumOKData$fileNameName[sumOKData$isBoth %in% 2],]
-
+# 3. Keep plates where at least Mouse primers worked (our amplification control)
+OKData[!is.na(OKData$Target.SYBR) & OKData$Target.SYBR == "mouse" & is.na(OKData$Ct.Mean.SYBR),]
 myTiles(OKData)
+
+mouseData <- OKData[OKData$Target.SYBR %in% "mouse" & OKData$No..Tm.SYBR %in% 1,]
+eimeriaData <- OKData[OKData$Target.SYBR %in% "eimeria",]
+
+toKeep
+
+mouseData
 
 # 4. Eventually, remove duplicates on different plates (based on sd)
 OKData$nameTarget <- paste(OKData$Name, OKData$Target.SYBR)
@@ -215,15 +159,10 @@ sumOKData <- OKData %>%
   data.frame()
 
 myTiles(OKData)
-# No melt curves for:
-# QPCR04.04.2018.XLS.csv but redonne twice later
-# QPCR30.05.2018.XLS.csv --> check manually the look of the curves. Keep for later
-# QPCR311-317.XLS.csv --> check manually the look of the curves. Keep for later
-
 
 ##### Calculate delta ct #####
 
-calculateDeltqCt <- function(df){
+calculateDeltaCt <- function(df){
   # Keep one value per plate per fullName (so per triplicate)
   df <- df[!duplicated(df[c("fullName")]),]
   
@@ -234,11 +173,11 @@ calculateDeltqCt <- function(df){
   mergedData <- merge(sumDataEimeria, sumDataMouse, 
                       by = c("Mouse_ID", "fileName", "tissue"))
   
-  mergedData$deltaCt <- as.numeric(mergedData$Ct.Mean.SYBR.x) - as.numeric(mergedData$Ct.Mean.SYBR.y)
+  # mergedData$deltaCt <- as.numeric(mergedData$Ct.Mean.SYBR.x) - as.numeric(mergedData$Ct.Mean.SYBR.y)
   return(mergedData)
 }
 
-finalData <- calculateDeltqCt(OKData)
+finalData <- calculateDeltaCt(OKData)
 
 library(ggplot2)
 ggplot(finalData, aes(x = finalData$tissue, y = finalData$deltaCt)) +
