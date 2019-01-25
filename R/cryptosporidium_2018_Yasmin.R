dc9b7f2b06ba5f59f1462fe23f16748906d46ad7
Alice Balard <alice.balard@fu-berlin.de>
2019-01-25 17:00:50 +0100

    finish crypto map

--- R/cryptosporidium_2018_Yasmin.R
+++ R/cryptosporidium_2018_Yasmin.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(ggmap)
 library(viridis) # pretty colors
 library(ggrepel) # to make labels on plot
+library(png) # to add image on plot (mice)
 
 # Import data
 yasminData <- read.csv("../data/Cryptosporidium/cryptoDetection2018.csv") 
@@ -20,29 +21,68 @@ Yasdata <- fullYasminData[fullYasminData$qPCR_results == "TRUE",]
 
 # get a map
 area <- get_map(location =
-                  c(min(Yasdata$Longitude - 0.5),
-                    min(Yasdata$Latitude - 0.5),
-                    max(Yasdata$Longitude + 0.5),
-                    max(Yasdata$Latitude + 0.5)),
+                  c(min(Yasdata$Longitude - 1),
+                    min(Yasdata$Latitude - 1),
+                    max(Yasdata$Longitude + 1),
+                    max(Yasdata$Latitude + 1)),
                 source = "stamen", maptype= "toner-lite",
                 zoom = 8)
 
 #plot the map :
 
 # little trick for ggrepel
-rownames(fullYasminData) <- fullYasminData$Mouse_ID
-
+rownames(Yasdata) <- Yasdata$Mouse_ID
 
 ggmap(area) +
-  geom_point(data = Yasdata, 
-             aes(x = Longitude, y = Latitude, 
-                 fill = log10(Nbr_oocyst_MEAN + 1)),
-             pch = 21, size = 4, alpha = 0.8) +
   scale_fill_viridis(option="inferno") +
-  geom_label_repel(data = Yasdata, 
+  geom_label_repel(data = Yasdata,min.segment.length = .7,
                    arrow = arrow(length = unit(0.01, "npc"), 
                                  type = "closed", ends = "last"),
                    force = 10,
                    aes(x = Longitude, y = Latitude, 
                        label = rownames(Yasdata)),
-                       size = 3)
+                       size = 3) +
+  geom_point(data = Yasdata, 
+             aes(x = Longitude, y = Latitude, 
+                 fill = log10(Nbr_oocyst_MEAN + 1)),
+             pch = 21, size = 4) 
+
+## The most pretty plot EVER
+
+g <- ggmap(area) +
+  scale_fill_viridis(option="inferno") +
+  geom_label_repel(
+    aes(x = Longitude, y = Latitude, 
+        fill = Nbr_oocyst_MEAN,
+        label = rownames(subset(Yasdata, Longitude < 14))),
+    data          = subset(Yasdata, Longitude < 14),
+    color         = "grey",
+    nudge_x       = 3 - subset(Yasdata, Longitude < 14)$Longitude,
+    segment.size  = 0.2,
+    direction     = "y",
+    hjust         = 0,
+    segment.colour = "black"
+    ) +
+  geom_label_repel(
+    aes(x = Longitude, y = Latitude, 
+        fill = Nbr_oocyst_MEAN,
+        label = rownames(subset(Yasdata, Longitude >= 14))),
+    data          = subset(Yasdata, Longitude >= 14),
+    color         = "lightgrey",
+    nudge_x       = 3 + subset(Yasdata, Longitude >= 14)$Longitude,
+    segment.size  = 0.2,
+    direction     = "y",
+    hjust         = 0,
+    segment.colour = "black"
+  ) 
+
+mice <- readPNG("mousepic.png")
+
+for (i in 1:nrow(Yasdata)) { 
+  g <- g + inset_raster(mice, 
+                        xmin = Yasdata$Longitude[i]- 0.18, 
+                        xmax = Yasdata$Longitude[i]+ 0.18,
+                        ymin = Yasdata$Latitude[i]- 0.05, 
+                        ymax = Yasdata$Latitude[i]+ 0.05, 
+                        interpolate = TRUE) }
+g
