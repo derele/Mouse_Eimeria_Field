72f5a205281d3553f8a3b1e5a39e66cf169fa167
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-09-30 22:43:02 +0200

    Update HZ19_FACS.R

--- R/Data_Analysis_Luke/HZ19_FACS.R
+++ R/Data_Analysis_Luke/HZ19_FACS.R
@@ -10,60 +10,19 @@ library(ggeffects)
 library(multcomp)
 library(fitdistrplus)
 
-CELLSfileUrl <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Field_data/HZ19_FACS_complete.csv"
-CELLS <- read.csv(text=getURL(CELLSfileUrl))
+cellsfileUrl <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Field_data/HZ19_FACS_complete.csv"
+cells <- read.csv(text=getURL(cellsfileUrl))
 
 #rename columns sensibly
-names(CELLS) <- c("Sample","ThCD4p","TcCD8p","Th1IFNgp_in_CD4p","Th17IL17Ap_in_CD4p",
-                "Tc1IFNgp_in_CD8p","Treg_Foxp3_in_CD4p","Dividing_Ki67p_in_Foxp3p",
-                "RORgtp_in_Foxp3p","Th1Tbetp_in_CD4pFoxp3n","Dividing_Ki67p_in_Tbetp",
-                "Th17RORgp_in_CD4pFoxp3n","Dividing_Ki67p_in_RORgtp")
+names(cells) <- c("Sample","CD4p", "Foxp3p_in_CD4p(Treg)", "Ki67p_in_CD4p_Foxp3p(dividing_Treg)","RORgtp_in_CD4p_Foxp3p(Treg17)","Scatter",
+                "Tbetp_in_CD4p_Foxp3n(Th1)", "Ki67_in_CD4p_Foxp3n_Tbetp(dividing_Th1)","RORgtp_in_CD4p_Foxp3n(Th17)",
+                "Ki67p_in_CD4p_Foxp3n_RORgtp(dividing_Th17)", "CD8p","Tbetp_in_CD8p(activated_CD8p)",
+                "Ki67p_in_CD8p_Tbetp(dividing_activated_CD8p)", "IFNgp_in_CD4p","IL17Ap_in_CD4p", "IFNgp_in_CD8p")
 
-#extract Mouse_ID from that mess and paste in "LM02" to standardize with our data structure
-CELLS$EH_ID <- gsub("\\d+: (mLN|spleen) (\\d{2})_\\d{3}_\\.fcs", "LM02\\2", CELLS$sample)
-CELLS$tissue <- gsub("\\d+: (mLN|spleen) (\\d{2})_\\d{3}.fcs", "\\1", CELLS$sample)
-
-#need to make caculations for each row per cell subset from the originating one
-#total cell counts subset to thCD4p and TcCD8p
-cell.counts$ThCD4p.cells <- with(cell.counts, (cell_counts/100)*ThCD4p)
-cell.counts$TcCD8p.cells <- with(cell.counts, (cell_counts/100)*TcCD8p)
-
-#ThCD4p subsets to Th1IFNgp_in_CD4p, Th17IL17Ap_in_CD4p, Treg_Foxp3_in_CD4p
-cell.counts$Th1IFNgp_in_CD4p.cells <- with(cell.counts, (ThCD4p.cells/100)*Th1IFNgp_in_CD4p)
-cell.counts$Th17IL17Ap_in_CD4p.cells <- with(cell.counts, (ThCD4p.cells/100)*Th17IL17Ap_in_CD4p)
-cell.counts$Treg_Foxp3_in_CD4p.cells <- with(cell.counts, (ThCD4p.cells/100)*Treg_Foxp3_in_CD4p)
-
-#TcCD8p subsets to Tc1IFNgp_in_CD8p
-cell.counts$Tc1IFNgp_in_CD8p.cells <- with(cell.counts, (TcCD8p.cells/100)*Tc1IFNgp_in_CD8p)
-
-#Treg_Foxp3_in_CD4p subsets to RORgtp_in_Foxp3p
-cell.counts$RORgtp_in_Foxp3p.cells <- with(cell.counts, (Treg_Foxp3_in_CD4p.cells/100)*RORgtp_in_Foxp3p)
-
-#RORgtp_in_Foxp3p subsets to Dividing_Ki67p_in_Foxp3p and Dividing_Ki67p_in_RORgtp
-cell.counts$Dividing_Ki67p_in_Foxp3p.cells <- with(cell.counts, (RORgtp_in_Foxp3p.cells/100)*Dividing_Ki67p_in_Foxp3p)
-cell.counts$Dividing_Ki67p_in_RORgtp.cells <- with(cell.counts, (RORgtp_in_Foxp3p.cells/100)*Dividing_Ki67p_in_RORgtp)
-
-#ThCD4p_Foxp3n (doesn't exist here) subsets to Th1Tbetp_in_CD4pFoxp3n, Th17RORgp_in_CD4pFoxp3n
-cell.counts$ThCD4p_Foxp3n <- with(cell.counts, (100-(Th17IL17Ap_in_CD4p + Th1IFNgp_in_CD4p + Treg_Foxp3_in_CD4p)))
-cell.counts$ThCD4p_Foxp3n.cells <- with(cell.counts, (ThCD4p.cells - Treg_Foxp3_in_CD4p))
-cell.counts$Th1Tbetp_in_CD4pFoxp3n.cells <- with(cell.counts, (ThCD4p_Foxp3n.cells/100)*Th1Tbetp_in_CD4pFoxp3n)
-cell.counts$Th17RORgp_in_CD4pFoxp3n.cells <- with(cell.counts, (ThCD4p_Foxp3n.cells/100)*Th17RORgp_in_CD4pFoxp3n)
-
-#Th1Tbetp_in_CD4pFoxp3n subsets to Dividing_Ki67p_in_Tbetp
-cell.counts$Dividing_Ki67p_in_Tbetp.cells <- with(cell.counts, (Th1Tbetp_in_CD4pFoxp3n.cells/100)*Dividing_Ki67p_in_Tbetp)
-
-#clean up table before saving
-#remove mouse column X (artifact)
-cell.counts$X = NULL
-cell.counts[,4] = NULL # only because of identical column names
-
-#write the .csv (rewrite Hongweis raw (all data included)) 
-write.csv(cell.counts, "./Documents/Eimeria_Lab/data/3_recordingTables/E7_112018_Eim_FACS_cell_counts_processed.csv", quote = FALSE)
-
-####################################################################################################################################
-#read in cell counts (FACS) data (clean)
-cell.countsURL <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Field_data/HZ19_FACS_complete.csv"
-cell.counts <- read.csv(text = getURL(cell.countsURL))
+#extract Mouse_ID from that mess and paste in "LM0" to standardize with our data structure
+# broken atm
+cells$Mouse_ID <- gsub("\\d+: (mLN|spleen)(_\\d{3})_\\d{3}\\.fcs", "AA_0\\2", cells$Sample)
+cells$tissue <- gsub("\\d+: (mLN|spleen)_(\\d{3})_\\d{3}.fcs", "\\1", cells$Sample)
 
 #####################################################################################################################################
 #introduce Dissections data
@@ -71,15 +30,16 @@ dissURL <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/ma
 Dissections <- read.csv(text=getURL(dissURL))
 
 #merge FACS with para data
-E7 <- merge(E7[E7$dpi%in%8,], cell.counts, by = "EH_ID")
-#include combined infection history
-E7$infHistory <- E7$primary:E7$challenge
+HZ19 <- merge(Dissections, cell.counts, by = "Mouse_ID")
+
+#remove percentages from cell columns
+
 
 ##select cell population names (now using .cells to calculate with actual cell populations)
-facs.measure.cols <- c("ThCD4p", "TcCD8p", "Th1IFNgp_in_CD4p", "Th17IL17Ap_in_CD4p", 
-                       "Tc1IFNgp_in_CD8p", "Treg_Foxp3_in_CD4p", "Dividing_Ki67p_in_Foxp3p", 
-                       "RORgtp_in_Foxp3p", "ThCD4p_Foxp3n", "Th1Tbetp_in_CD4pFoxp3n", "Dividing_Ki67p_in_Tbetp", 
-                       "Th17RORgp_in_CD4pFoxp3n", "Dividing_Ki67p_in_RORgtp")
+facs.measure.cols <- c("Sample","CD4p", "Foxp3p_in_CD4p(Treg)", "Ki67p_in_CD4p_Foxp3p(dividing_Treg)","RORgtp_in_CD4p_Foxp3p(Treg17)","Scatter",
+                       "Tbetp_in_CD4p_Foxp3n(Th1)", "Ki67_in_CD4p_Foxp3n_Tbetp(dividing_Th1)","RORgtp_in_CD4p_Foxp3n(Th17)",
+                       "Ki67p_in_CD4p_Foxp3n_RORgtp(dividing_Th17)", "CD8p","Tbetp_in_CD8p(activated_CD8p)",
+                       "Ki67p_in_CD8p_Tbetp(dividing_activated_CD8p)", "IFNgp_in_CD4p","IL17Ap_in_CD4p", "IFNgp_in_CD8p")
 
 #test for normality
 #if(!require(devtools)) install.packages("devtools")
