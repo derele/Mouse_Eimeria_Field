2d168409058ba0fd82bad77c4177595a7634d2e4
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-10-18 16:27:43 +0200

    upload first RT-qPCR and sample select script update

--- R/Data_Analysis_Luke/HZ16-17_RTqPCR.R
+++ R/Data_Analysis_Luke/HZ16-17_RTqPCR.R
@@ -3,20 +3,47 @@ library(RCurl)
 library(tidyverse)
 library(Rmisc)
 
-#read in 2016-2017 qPCRs
+#read in 2016-2017 qPCRs (based on threshold, check emlting curves too)
 PCR <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Eimeria_detection/FINALqpcrData_2016_2017_threshold5.csv"
 PCR <- read.csv(text = getURL(PCR))
-# select positive samples
-positive2016 <- filter(PCR, qPCRstatus == "positive", year == 2016, qPCRsummary == "infected cecum")
-positive2017 <- filter(PCR, qPCRstatus == "positive", year == 2017, qPCRsummary == "infected cecum")
+# select positive samples 2016
+positive2016a <- filter(PCR, qPCRstatus == "positive", year == 2016, qPCRsummary == "infected cecum")
+positive2016b <- filter(PCR, qPCRstatus == "positive", year == 2016, qPCRsummary == "cecum stronger")
+positive2016c <- filter(PCR, qPCRstatus == "positive", year == 2016, qPCRsummary == "ileum stronger")
+# select positive samples 2017
+positive2017a <- filter(PCR, qPCRstatus == "positive", year == 2017, qPCRsummary == "infected cecum")
+positive2017b <- filter(PCR, qPCRstatus == "positive", year == 2017, qPCRsummary == "cecum stronger")
+positive2017c <- filter(PCR, qPCRstatus == "positive", year == 2017, qPCRsummary == "ileum stronger")
 # select negative samples
 negative2016 <- filter(PCR, qPCRstatus == "negative", year == 2016, qPCRsummary == "non infected")
 negative2017 <- filter(PCR, qPCRstatus == "negative", year == 2017, qPCRsummary == "non infected")
-# randomly select negative samples (21 per 2016, 29 per 2017)
-negative2016 <- sample_n(negative2016, 21)
-negative2017 <- sample_n(negative2017, 29)
-#merge and save
+# randomly select negative samples (28 per 2016, 29 per 2017)
+negative2016 <- sample_n(negative2016, 28)
+negative2017 <- sample_n(negative2017, 33)
 negative <- bind_rows(negative2016, negative2017)
-positive <- bind_rows(positive2016, positive2017)
-HZ16and17 <- bind_rows(negative, positive)
-write.csv(HZ16and17, "//home/luke/Repositories/Mouse_Eimeria_Databasing/Mouse_Eimeria_Databasing/data/HZ16and17RT-qPCRsample_list.csv")
+#merge
+positiveA <- bind_rows(positive2016a, positive2017a)
+positiveB <- bind_rows(positive2016b, positive2017b)
+positiveC <- bind_rows(positive2016c, positive2017c)
+positiveAB <- bind_rows(positiveA, positiveB)
+positive <- bind_rows(positiveAB, positiveC)
+dplyr::arrange(positive, year)
+HZ16and17 <- rbind(positive, negative)
+############################## Add oocyst data
+oocysts <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Eimeria_detection/Eimeria_oocysts_2015%262017_Lorenzo.csv"
+oocysts <- read.csv(text = getURL(oocysts))
+oocysts <- select(oocysts, Mouse_ID, mean_neubauer, OPG)
+HZ16and17 <- merge(oocysts, HZ16and17, by = "Mouse_ID", all.y = TRUE)
+# check for oocyst positive and qPCR negative flukes (keeps reducing the overall number... maybe stick to oocyst and HZ merge)
+DoublePositive <- dplyr::filter(HZ16and17, qPCRstatus == "positive", OPG > 0)
+DoublePositive$positive <- "double"
+OocystPositive <- dplyr::filter(HZ16and17, qPCRstatus == "negative", OPG > 0)
+OocystPositive$positive <- "oocyst"
+qPCRPositive <- dplyr::filter(HZ16and17, qPCRstatus == "positive", OPG == 0)
+qPCRPositive$positive <- "qPCR"
+PositiveInvestigate <- rbind(DoublePositive, OocystPositive, qPCRPositive)
+PositiveInvestigate <- distinct(PositiveInvestigate)
+HZ16and17 <- merge(HZ16and17, PositiveInvestigate, by = "Mouse_ID")
+
+################### Add melting cuve analysis data #######################
+
