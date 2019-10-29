6975c1ac4b342102ceb8fa183ad542eca25d2026
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-10-29 15:15:56 +0100

    select samples all wild

--- R/Data_Analysis_Luke/HZ16-17_RTqPCR.R
+++ R/Data_Analysis_Luke/HZ16-17_RTqPCR.R
@@ -2,10 +2,31 @@ library(httr)
 library(RCurl)
 library(tidyverse)
 library(Rmisc)
+library(purrr)
 
 #read in 2016-2017 qPCRs (based on threshold, check emlting curves too)
 Mice <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/MiceTable_fullEimeriaInfos_2014to2017.csv"
 Mice <- read.csv(text = getURL(Mice))
+# reduce this monstrosity
+Mice <- dplyr::select(Mice, Mouse_ID, Sex, Longitude, Latitude, Year, HI_NLoci, HI, Body_weight, Body_length, Species,
+                      Spleen_mass, Status, mean_neubauer, OPG, delta_ct_cewe_MminusE, eimeriaSpecies)
+E64 <- dplyr::filter(Mice, eimeriaSpecies == "E_ferrisi")
+E88 <- dplyr::filter(Mice, eimeriaSpecies == "E_falciformis")
+Double <- dplyr::filter(Mice, eimeriaSpecies == "Double")
+Double_tbd <- dplyr::filter(Mice, eimeriaSpecies == "Double_tbd")
+Double_det <- dplyr::filter(Mice, eimeriaSpecies == "Double_ferrisi_vermiformis")
+Other <- dplyr::filter(Mice, eimeriaSpecies == "Other")
+Negative <- dplyr::filter(Mice, eimeriaSpecies == "Negative")
+
+Positive <- rbind(E64, E88)
+Positive <- rbind(Positive, Double)
+Positive <- rbind(Positive, Double_tbd)
+Positive <- rbind(Positive, Double_det)
+Positive <- rbind(Positive, Other)
+
+
+
+data.frame(E64, E88, Double, Double_tbd, Double_det, Other) %>% reduce(inner_join, by = "Mouse_ID")
 
 # select positive samples 2016
 positive2016a <- filter(PCR, qPCRstatus == "positive", year == 2016, qPCRsummary == "infected cecum")
