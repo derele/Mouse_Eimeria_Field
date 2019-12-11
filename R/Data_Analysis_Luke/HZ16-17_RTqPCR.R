fbc7feb1394b67a1d7b81037737fc823227617e3
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2019-12-11 13:27:42 +0100

    Upload HZ16-17 RT-qPCRs + script changes

--- R/Data_Analysis_Luke/HZ16-17_RTqPCR.R
+++ R/Data_Analysis_Luke/HZ16-17_RTqPCR.R
@@ -5,27 +5,8 @@ library(tidyverse)
 library(purrr)
 library(ggplot2)
 
-#read in 2016-2017 qPCRs (based on threshold, check melting curves too)
-Mice <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/MiceTable_fullEimeriaInfos_2014to2017.csv"
-Mice <- read.csv(text = getURL(Mice))
-# reduce this monstrosity
-Mice <- dplyr::select(Mice, Mouse_ID, Sex, Longitude, Latitude, Year, HI_NLoci, HI, Body_weight, Body_length, Species,
-                      Spleen_mass, Status, mean_neubauer, OPG, delta_ct_cewe_MminusE, eimeriaSpecies, observer)
-E64 <- dplyr::filter(Mice, eimeriaSpecies == "E_ferrisi")
-E88 <- dplyr::filter(Mice, eimeriaSpecies == "E_falciformis")
-Double <- dplyr::filter(Mice, eimeriaSpecies == "Double")
-Double_tbd <- dplyr::filter(Mice, eimeriaSpecies == "Double_tbd")
-Double_det <- dplyr::filter(Mice, eimeriaSpecies == "Double_ferrisi_vermiformis")
-Other <- dplyr::filter(Mice, eimeriaSpecies == "Other")
-Negative <- dplyr::filter(Mice, eimeriaSpecies == "Negative")
+# load in sample list
 
-Positive <- rbind(E64, E88)
-Positive <- rbind(Positive, Double)
-Positive <- rbind(Positive, Double_tbd)
-Positive <- rbind(Positive, Double_det)
-Positive <- rbind(Positive, Other)
-
-write.csv(Positive, "~/Mouse_Eimeria_Databasing/data/Eimeria_detection/HZ14-17_positives.csv")
 
 ### Check against MC analysis (Lorenzo)
 LorenzoMC <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/Eimeria_detection/HZ16-17_InfInt_MC_Lorenzo%26Mert.csv"
