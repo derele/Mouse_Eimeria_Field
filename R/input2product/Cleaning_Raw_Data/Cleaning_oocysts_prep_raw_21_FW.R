5e0077660570342fe8c08b6eccad0cd74c3d9c60
fayweb <fy.webster@gmail.com>
2022-02-22 17:53:37 +0100

    in this directory I will be saving all rfiles used to prepare and clean the raw data

---
+++ R/input2product/Cleaning_Raw_Data/Cleaning_oocysts_prep_raw_21_FW.R
@@ -0,0 +1,5 @@
+library(readxl)
+
+HZ21_oocysts <- read.csv("Mouse_Eimeria_Field/data_input/HZ21_Oocysts.csv")
+
+sota <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Field/master/data_products/SOTA_Data_Product.csv")
