a6604634c4d4a2fa0445c4d4de9b20766ce9d67e
fayweb <74728576+fayweb@users.noreply.github.com>
2022-02-23 17:31:22 +0100

    Delete Cleaning_oocysts_prep_raw_21_FW.R

--- R/input2product/Cleaning_oocysts_prep_raw_21_FW.R
+++
@@ -1,5 +0,0 @@
-library(readxl)
-
-HZ21_oocysts <- read.csv("Mouse_Eimeria_Field/data_input/HZ21_Oocysts.csv")
-
-sota <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Field/master/data_products/SOTA_Data_Product.csv")
