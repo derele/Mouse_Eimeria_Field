b22d97ed1853cc68b8ff29796463cd8f668c94b2
fayweb <fy.webster@gmail.com>
2022-03-07 17:23:54 +0100

    added the last qpcr data

--- R/raw2input/Cleaning_Raw_Data/Reading_qPCR_Data_faeces.R
+++ R/raw2input/Cleaning_Raw_Data/Reading_qPCR_Data_faeces.R
@@ -8,7 +8,8 @@ library(readr)
 #the result files are located at: GitHub/Mouse_Eimeria_Field/data_input/qPCR_faeces/Results
 
 #change back to the github repository (Change to where your github repository is located)
-setwd("~/Documents/GitHub/")
+setwd("~/GitHub/")
+
 #1 change the current working directory to the location where the qPCR data files are
 setwd("Mouse_Eimeria_Field/data_input/qPCR_faeces/Results/Results_files/")
 
@@ -19,11 +20,14 @@ NT <- read.csv("https://raw.githubusercontent.com/derele/Mouse_Eimeria_Field/mas
 NT_list <- as.list(NT$qPCR.Results.file.names)
 
 #change back to the github repository (Change to where your github repository is located)
-setwd("~/Documents/GitHub/")
+setwd("~/GitHub/")
+
+#change back to the github repository (Change to where your github repository is located)
+setwd("Mouse_Eimeria_Field/data_input/qPCR_faeces/Results/Results_files")
 
 #write a function to specify how to read the qPCR files
 read_qPCR_file <- function(x) {
-  setwd("Mouse_Eimeria_Field/data_input/qPCR_faeces/Results/Results_files")
+  
   df1 <- read.xlsx(x, sheetIndex = 1)
   #remove unecessary rows of the data frame
   #everything before actual data
@@ -33,6 +37,7 @@ read_qPCR_file <- function(x) {
   colnames(df1) <- df1[1, ]
   #Now remove the first row
   df1 <- df1 %>% filter(!row_number() %in% 1)
+  
 }
 
 
@@ -45,9 +50,11 @@ list_results <- lapply(NT_list, read_qPCR_file)
 #show the data frame consisting of each result data file
 df_results <- Reduce(rbind, list_results)
 
+df_results <- unique(df_results)
+
 #change your working directory
 setwd("~/GitHub/Mouse_Eimeria_Field")
 
 #write the data frame in a csv file 
-write.csv(df_results, "data_products/qPCR_faeces_2022", row.names=FALSE)
+write.csv(df_results, "data_products/qPCR_faeces_field_eimeria_22", row.names=FALSE)
 
